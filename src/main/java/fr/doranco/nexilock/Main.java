bdd2841a6bdd81202457c64e9693bb9d4f4fa953
NoRushJustPanda <ethan@meideck.net>
2026-02-25 17:02:11 +0100

    feat: Adding login & password verification.

--- src/main/java/fr/doranco/nexilock/Main.java
+++ src/main/java/fr/doranco/nexilock/Main.java
@@ -8,4 +8,26 @@ public class Main {
         DatabaseManager.initDatabase();
         System.out.println("Système prêt et sécurisé");
     }
+
+    public static void loginUser(String username, String password) {
+        // Creating AuthService instance.
+        AuthService auth = new AuthService();
+        int attempt = 0;
+ 
+        // User login & password verification with 3 attempts.
+        while (attempt < 3)
+            if(auth.authenticate(username, password)){
+                System.out.println("Accès autorisé !");
+                break;
+            }
+            // Verification for empty inputs
+            else if(username.isBlank() || password.isBlank()) {
+                System.out.println("Veuillez entrez un identifiant et mot de passe valide !");
+                attempt = attempt + 1;
+            }
+            else{
+                System.out.println("Accès refusé, veuillez réesayer.");
+                attempt = attempt + 1;
+        }
+    }
 }
